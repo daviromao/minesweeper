7d3c96063774572cce866030de8a35be124be817
daviromao <davi.romao0210@gmail.com>
2020-05-28 07:58:50 -0300

    feat: added mines and counted places

--- src/scripts/minesweeper.js
+++ src/scripts/minesweeper.js
@@ -1,6 +1,79 @@
 const height = 6;
 const width = 5;
 const totalMines = 10;
-const grid = Array(height).fill().map(() => Array(width).fill(0));
 
-console.log(grid);
+let grid = [];
+
+
+function start(){
+  createGridDataStructure();
+  shuffleGrid();
+  countAdjacentPlaces();
+  renderGrid();
+}
+
+function createGridDataStructure(){
+  grid = Array(height).fill().map(() => Array(width).fill(0));
+}
+
+function shuffleGrid(){
+  let mines = 0;
+  let x, y;
+
+  while(mines < totalMines){
+    generateRandomPosition()
+    if(!grid[y][x]){
+      grid[y][x] = -1;
+
+      mines++;
+    }
+  }
+
+  function generateRandomPosition(){
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+  }
+}
+
+function countAdjacentPlaces(){
+  for(let y = 0; y < height; y++){
+    for(let x = 0; x < width; x++){
+      if(!isMine(x, y)) continue;
+      
+      count(x-1, y-1); // NW
+      count(x, y-1);   //N
+      count(x+1, y-1); //NE
+      count(x+1, y);   //E
+      count(x+1, y+1); //SE
+      count(x, y+1);   //S
+      count(x-1, y+1); //SW
+      count(x-1, y);   //W
+
+    }
+  }
+
+  function isValidCount(x, y){
+    return ((x >= 0 && x < width) && (y >= 0 && y < height));
+  }
+
+  function isMine(x, y){
+    return grid[y][x] == -1;
+  }
+
+  function count(x, y){
+    if(isValidCount(x, y) && !isMine(x, y)){
+      grid[y][x]++;
+    }
+  }
+
+}
+
+function renderGrid(){
+  console.log(grid);
+}
+
+function map2(A, func){
+
+}
+
+start();
